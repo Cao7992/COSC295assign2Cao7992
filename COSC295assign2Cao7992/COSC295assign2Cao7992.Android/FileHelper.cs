e824196544bed4e2b0bcd558dc508ef434fa05fa
Chau Cao <cao7992@saskpolytech.ca>
2025-03-29 14:22:56 -0600

    Add project files.

---
+++ COSC295assign2Cao7992/COSC295assign2Cao7992.Android/FileHelper.cs
@@ -0,0 +1,30 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using COSC295assign2Cao7992;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using System.IO;
+using Xamarin.Forms;
+
+/**
+ * Helper method to get a full path of storage where the database located
+ */
+[assembly: Dependency(typeof(COSC295assign2Cao7992.Droid.FileHelper))]
+namespace COSC295assign2Cao7992.Droid
+{
+    public class FileHelper : IFileHelper
+    {
+        public string GetLocalFilePath(string filename)
+        {
+            string path = System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal);
+            return Path.Combine(path, filename);
+        }
+    }
+}
---
+++ COSC295assign2Cao7992/COSC295assign2Cao7992/App.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace COSC295assign2Cao7992
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+    /*
+     * Interface to get full path to storage location, depending on different platform
+     */
+    public interface IFileHelper
+    {
+        string GetLocalFilePath(string filename);
+    }
+}
